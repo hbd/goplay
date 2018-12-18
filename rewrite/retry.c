d6ff5f5e6c870f5ad50805b61c78ec1926dbc389
Zachary A. Keener <zakkeener@gmail.com>
2018-12-18 13:02:16 -0500

    Add interface to retry

--- rewrite/retry.c
+++ rewrite/retry.c
@@ -6,17 +6,45 @@
  */
 
 // Define an underlying slice struct.
-typedef  struct {
-  void** data; // Generic array.
-  size_t len; // size_t is an unsigned int >= 16 bits
-  size_t cap;
-}        s_slice;
+typedef   struct {
+  void**  data; // Generic array.
+  size_t  len; // size_t is an unsigned int >= 16 bits
+  size_t  cap;
+}         s_slice;
 
-// Define a slice, a pointer to a slice struct.
-typedef s_slice* slice;
+// Define a slice, a pointer to a s_slice struct.
+typedef   s_slice* slice;
 
 /*
  * !Slice.
+ */
+
+
+/*
+ * Interface.
+ */
+
+typedef   struct {
+  char**  name; // Name of the function.
+}         function;
+
+// itable (interface table) defines the lists the methods used to satisfy an interface.
+typedef     struct {
+  /* void*  type?    */   // type is ...?
+  /* void** function */   // functions
+}           itable;
+
+// Define an underlying slice struct.
+typedef  struct {
+  void*  type;  // Pointer to type in itable (interface table).
+  void*  data;  // Pointer to data.
+}        s_interface;
+
+// Define an interface, a pointer to an s_interface struct.
+typedef  s_interface* interface;
+
+/*
+ * !Interface.
  */
 
 /*
